20bf0db60171991d29e12c82015dbc0887e0639e
zhanglei <zhanglei1@rongcloud.cn>
2016-06-12 18:43:23 +0800

    Base version.

---
+++ app/src/main/java/demo/rong/livechat/LiveChatActivty.java
@@ -0,0 +1,13 @@
+package demo.rong.livechat;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LiveChatActivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ toolkit/src/main/java/io/rong/toolkit/MainActivity.java
@@ -0,0 +1,9 @@
+package io.rong.toolkit;
+
+import android.app.Activity;
+
+/**
+ * Created by Bob on 15/7/7.
+ */
+public class MainActivity extends Activity {
+}
